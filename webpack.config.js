7ddd2cfd8bda516a3371cdd1e99e7638cfb3a07e
nikhil <nikhil.gosavi@pentacor.de>
2021-06-27 11:04:30 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWekpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: 8080
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                products: 'products@http://localhost:8081/remoteEntry.js',
+                cart: 'cart@http://localhost:8082/remoteEntry.js'
+            }
+        }),
+       new HtmlWekpackPlugin({
+           template: './public/index.html'
+       })
+    ]
+}
